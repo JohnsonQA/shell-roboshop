27baa9307ee34ef6f7b5b8a6e435fef81eb35be3
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-21 21:48:48 +0530

    Configuring DNS records

--- catalogue.sh
+++ catalogue.sh
@@ -86,8 +86,15 @@ VALIDATE $? "Added mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installed Mongodb"
 
-mongosh --host mongodb.roboshop.space </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Loaded data"
+#To check wethere DB already exists or not 1 means exists lesser than 1 means not exists
+STATUS=$(mongosh --host mongodb.roboshop.space --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.roboshop.space </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loaded data"
+else
+    echo -e "Catalogue DB already exists... $M SKIPPING $N"
+fi
 
 
 
