b777297c64d2f5dfa9c6b2568aa32568df334cd3
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-21 21:15:21 +0530

    Configuring DNS records

---
+++ catalogue.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+M="\e[35m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)       #It will split the scriptName and gives only 10-logs which is field 1
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo -e "$M Script executing at : $N $(date)"  | tee -a $LOG_FILE
+
+if [ $USERID -eq 0 ]   
+then
+    echo -e "$M Running with sudo user... $N" | tee -a $LOG_FILE
+else
+    echo -e "$R Error:: Run with sudo user to install packages $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+#function to validate package installed succesfully or not
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabled existing nodejs version"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabled required nodejs version"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installed nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Created roboshop user"
+
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "Created app dir"
+
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloaded the catalogue service"
+
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Unzipped the catalogue service"
+
+cd /app
+npm install &>>$LOG_FILE
+VALIDATE $? "Installed npm pkgm"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "Catalogue service pasted in systemd"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Loaded the service"
+
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue &>>$LOG_FILE
+VALIDATE $? "Catalogue service started"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "Added mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installed Mongodb"
+
+mongosh --host mongodb.roboshop.space </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "Loaded data"
+
+
+
+
+
+
+
+
+
+
+
+
+
