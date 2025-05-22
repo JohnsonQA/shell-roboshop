65caad2ff500181fa6cc21009bf5d6724c004183
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-22 09:42:05 +0530

    Updated Start and end Time to scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -95,6 +95,11 @@ then
 else
     echo -e "Catalogue DB already exists... $M SKIPPING $N"
 fi
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
 
 
